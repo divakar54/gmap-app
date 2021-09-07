93af541c5d3baa17e943739399a15e798e00ede1
divakar10d <divakar10d@gmail.com>
2021-09-07 16:17:56 +0530

    added layout information in App.js

--- gmap-app/src/App.js
+++ gmap-app/src/App.js
@@ -1,10 +1,24 @@
 import './App.css';
+import { CssBaseline, Grid } from '@material-ui/core'; //base css for the app
 
+
+import Header from './components/Header/Header';
+import List from './components/List/List';
+import Map from './components/Map/Map';
 function App() {
   return (
-    <div className="App">
-      <h1>Hello</h1>
-    </div>
+    <>
+      <CssBaseline />
+      <Header />
+      <Grid container spacing={3} style={{ width : '100%' }}>
+          <Grid item xs={12} md={4}>
+            <List />
+          </Grid>
+          <Grid item xs={12} md={8}>
+            <Map />
+          </Grid>
+      </Grid>
+    </>
   );
 }
 
